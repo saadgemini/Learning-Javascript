3240a07cd04f5386ddcfe712bbf686eb71fcb072
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-05 13:39:02 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1234
+let accountEmail = "saad@gmail.com"
+var accountPassword = "asd123"
+accountCity = "Mau"
+
+let accountState;
+
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// accountId = 500; // we can not change the value of const
+
+// console.log(accountId);
+
+// accountEmail = "hi@gmail.com" //we can change values of let
+
+// console.log(accountEmail);
+
+// accountCity = "Delhi"
+
+// console.log(accountCity);
+
+// console.log(accountState);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
