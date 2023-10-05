9ba6152870249c969d9654cb9f8e8c6ab777814a
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-05 14:39:10 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict"; //treat all JS code as a newer version
+
+//  alert(3 + 4)    We are using NodeJS, not browser
+
+console.log(3 
+    + 3)        //code readability should be high
+
+console.log("Saad");
+
+let name = "Saad"
+
+let age = 27
+
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+// object
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof null);           // dataType of null => Object
+
+console.log(typeof undefined);      // dataType of undefinef => Undefined
