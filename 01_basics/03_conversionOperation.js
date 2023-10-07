a1027f50289c3902a5e08c0ed59b59b9358d0b41
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-07 05:42:50 +0000

    saad

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,6 +35,61 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// **************** Operation ********************
+
+let value = 3;
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+
+// console.log(2 - 2);
+
+// console.log(2 * 2);
+
+// console.log(2 ** 3);
+
+// console.log(2 / 2);
+
+// console.log(2 % 3);
+
+let str1 = "hi"
+
+let str2 = " Saad"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 5 * 3 % 11);
+
+// console.log((3 + 5) * 3 % 11);
+
+
+// ********** Prefix/Postfix *********
+
+let x = 8;
+let y = ++x;
+
+console.log(x);
+console.log(y);
+
+let a = 8;
+let b = a++;
+
+console.log(a);
+console.log(b);
