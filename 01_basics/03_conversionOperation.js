0a40a55b718f4e05895ec123f6fe357327518842
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-07 05:02:02 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,40 @@
-let score = 45
+let score = false
 
-console.log(typeof score);
+// console.log(typeof score);
 
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// 45 => 45
+// "45asd" =>  NaN
+// true => 1    false => 0
+
+let isLoggedIn = 100
+
+// console.log(typeof isLoggedIn);
+
+// console.log(isLoggedIn);
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true (Any number other than 0 gives TRUE)
+// 0 => false
+// "" => false
+// "Saad" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
