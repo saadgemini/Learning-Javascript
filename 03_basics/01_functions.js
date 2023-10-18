cbd8c2b63ebebd277e70f666f57cd9f02ca6d656
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-18 17:58:58 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,33 @@
+// console.log("S");
+// console.log("A");
+// console.log("A");
+// console.log("D");
+
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("A");
+    console.log("D");
+}
+
+// sayMyName()
+
+function addTwoNumbers(num1, num2){
+    console.log(num1 + num2);
+}
+
+// addTwoNumbers(2, 5)         // 7
+// addTwoNumbers(2, "a")       // 2a
+// addTwoNumbers("b", 9)       // b9
+// addTwoNumbers("c", "a")     // ca
+// addTwoNumbers(2, null)      // 2
+// addTwoNumbers(2, undefined) // NaN
+
+
+function addTwoNums(num3, num4){
+    console.log(num3 + num4); 
+}
+
+let result = addTwoNums(7, 9)       // 16
+console.log("Result ", result);     // undefined
+
