4b511873808d3b62bee8bfa20bdc0933a3a3c344
Saad Ansari <94887530+saadgemini@users.noreply.github.com>
2023-10-21 06:13:07 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,10 +24,32 @@ function addTwoNumbers(num1, num2){
 // addTwoNumbers(2, undefined) // NaN
 
 
+// function addTwoNums(num3, num4){
+//     // console.log(num3 + num4);
+// }
+
+// let result = addTwoNums(7, 9)       // 16
+// console.log("Result ", result);     // undefined => console only print the values, not return the value
+
+
 function addTwoNums(num3, num4){
-    console.log(num3 + num4); 
+    // let result = num3 + num4
+    // return result
+    // console.log("SAAD");        // after return keyword no other code is executed
+
+    return num3 + num4
 }
 
-let result = addTwoNums(7, 9)       // 16
-console.log("Result ", result);     // undefined
+// let result = addTwoNums(7, 9)       
+// console.log("Result ", result);
+
+function loginUserMessage(username = "Harry"){      // we can give some default value to avoid if condition
+    if (!username) {
+        console.log("Pleasen enter username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Saad"));
 
